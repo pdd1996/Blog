b1fd13427c7a75f0b59650790f5b867fac2a9b8c
paddy <pdd337816464@163.com>
2024-07-06 19:58:15 +0800

    todo getInfo

--- admin/src/main/java/com/paddy/controller/LoginController.java
+++ admin/src/main/java/com/paddy/controller/LoginController.java
@@ -1,20 +1,34 @@
 package com.paddy.controller;
 
 import com.paddy.domain.ResponseResult;
+import com.paddy.domain.entity.LoginUser;
 import com.paddy.domain.entity.User;
+import com.paddy.domain.vo.AdminUserInfoVo;
+import com.paddy.domain.vo.UserInfoVo;
 import com.paddy.enums.AppHttpCodeEnum;
 import com.paddy.exception.SystemException;
 import com.paddy.service.LoginService;
+import com.paddy.service.MenuService;
+import com.paddy.service.RoleService;
+import com.paddy.utils.BeanCopyUtils;
+import com.paddy.utils.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class LoginController {
     @Autowired
     private LoginService loginService;
+    @Autowired
+    private MenuService menuService;
+    @Autowired
+    private RoleService roleService;
     @PostMapping("/user/login")
     public ResponseResult login(@RequestBody User user){
         //如果用户在进行登录时，没有传入'用户名'
@@ -23,6 +37,24 @@ public class LoginController {
             throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
         }
         return loginService.login(user);
+    }
+    @GetMapping("/getInfo")
+    public ResponseResult<AdminUserInfoVo> getInfo(){
+        // 获取当前登陆的用户
+        LoginUser loginUser = SecurityUtils.getLoginUser();
+        // 根据用户id查询权限信息
+        List<String> perms = menuService.selectPermsByUserId(loginUser.getUser().getId());
+        // 根据用户id查询角色信息
+        List<String> roleKeyList = roleService.selectRoleKeyByUserId(loginUser.getUser().getId());
+
+        // 获取用户信息
+        User user = loginUser.getUser();
+        UserInfoVo userInfoVo = BeanCopyUtils.copyBean(user, UserInfoVo.class);
+
+        // 封装数据返回
+        AdminUserInfoVo adminUserInfoVo = new AdminUserInfoVo(perms, roleKeyList, userInfoVo);
+
+        return ResponseResult.okResult(adminUserInfoVo);
     }
 
 }
---
+++ common/src/main/java/com/paddy/domain/vo/AdminUserInfoVo.java
@@ -0,0 +1,20 @@
+package com.paddy.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdminUserInfoVo {
+    private List<String> permissions;
+
+    private List<String> roles;
+
+    private UserInfoVo user;
+}
